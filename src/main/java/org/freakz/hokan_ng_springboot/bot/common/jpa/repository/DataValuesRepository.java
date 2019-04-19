5e78a1438dba14160a26d0648bb617a8afe75ad8
Petri Airio <petri.airio@valamis.com>
2019-04-19 23:08:35 +0300

    Glugga count3

--- src/main/java/org/freakz/hokan_ng_springboot/bot/common/jpa/repository/DataValuesRepository.java
+++ src/main/java/org/freakz/hokan_ng_springboot/bot/common/jpa/repository/DataValuesRepository.java
@@ -3,11 +3,15 @@ package org.freakz.hokan_ng_springboot.bot.common.jpa.repository;
 import org.freakz.hokan_ng_springboot.bot.common.jpa.entity.DataValues;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Created by Petri Airio on 31.8.2015.
  */
 public interface DataValuesRepository extends JpaRepository<DataValues, Long> {
 
-    DataValues findByNickAndNetworkAndKeyName(String nick, String network, String keyName);
+    DataValues findByNickAndChannelAndNetworkAndKeyName(String nick, String channel, String network, String keyName);
+
+    List<DataValues> findAllByChannelAndNetworkAndKeyName(String channel, String network, String keyName);
 
 }
